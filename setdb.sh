b726ce7f10e3ab7faa471dfd5186b8d85c1300ac
Shirsendu Das <37840366+DarkenLight@users.noreply.github.com>
2022-03-27 19:55:30 +0530

    Update setdb.sh

--- setdb.sh
+++ setdb.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
-find .env -type f -exec sed -i '' -e "/^DB_HOST=/s/=.*/=192.168.1.5/" {} \;
-find .env -type f -exec sed -i '' -e "/^DB_DATABASE=/s/=.*/=firefly/" {} \;
-find .env -type f -exec sed -i '' -e "/^DB_USERNAME=/s/=.*/=firefly/" {} \;
-find .env -type f -exec sed -i '' -e "/^DB_PASSWORD=/s/=.*/=firefly/" {} \;
+
+echo "Enter Database Hostname or IP Address: "
+read db_hostname
+echo "Enter Database Name: "
+read db_name
+echo "Enter Database Username: "
+read db_username
+echo "Enter Database Password: "
+read db_password
+
+echo "Entered Details are Below"
+echo "=============================="
+echo "= DB_HOSTNAME = $db_hostname"
+echo "= DB_DATABASE = $db_name"
+echo "= DB_USERNAME = $db_username"
+echo "= DB_PASSWORD = $db_password"
+echo "=============================="
+
+find .env -type f -exec sed -i '' -e "/^DB_HOST=/s/=.*/=$db_hostname/" {} \;
+find .env -type f -exec sed -i '' -e "/^DB_DATABASE=/s/=.*/=$db_name/" {} \;
+find .env -type f -exec sed -i '' -e "/^DB_USERNAME=/s/=.*/=$db_username/" {} \;
+find .env -type f -exec sed -i '' -e "/^DB_PASSWORD=/s/=.*/=$db_password/" {} \;
